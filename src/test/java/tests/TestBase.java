1080f218218d0c48b789b29aa62f3d5938a1c62e
Irina Molodtsova <irinamolodtsova98@gmail.com>
2025-05-29 13:13:27 +0300

    diploma

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,6 +10,7 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -23,6 +24,8 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments("--user-data-dir=/tmp/chrome-tmp-" + System.currentTimeMillis());
         Configuration.pageLoadStrategy = "eager";
         Configuration.pollingInterval = 400;
         Configuration.baseUrl = config.getBaseUrl();
